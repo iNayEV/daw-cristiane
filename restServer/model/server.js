b55b8ff4a90f06603676a33bdb44b083b51354ee
super <cespin@insdanielblanxart.cat>
2021-10-07 15:18:45 +0200

    a

---
+++ restServer/model/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuarisPath = "/api/";
+
+    // Middlewares
+    this.middlewares();
+
+    // Rutes de l'aplicació
+    this.routes();
+  }
+
+  middlewares() {
+    // Lectura i parseig del body
+    this.app.use(express.json());
+
+    // Directori public
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.use(this.usuarisPath, require("../routes/usuaris"));
+  }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor en el port", this.port);
+    });
+  }
+}
+
+module.exports = Server;
